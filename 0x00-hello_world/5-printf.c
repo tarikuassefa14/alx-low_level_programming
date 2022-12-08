a643e2ff91c7f2b2f7c55234fd693b49168a4472
tarikuassefa14 <tarikuassefa14@gmail.com>
2022-12-08 06:07:30 -0800

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,11 @@
-
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - Entery points
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-
-        return (0);
+	printf("with proper grammar, but the coutcome is piece of art,\n");
+	return (0);
 }
-
