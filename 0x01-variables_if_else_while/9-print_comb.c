e3f4f44c855c840bcd428a2e91319668e2acf0a1
tarikuassefa14 <tarikuassefa14@gmail.com>
2022-12-09 09:07:43 -0800

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase,
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 48; d < 58; d++)
+	{
+		putchar(d);
+		if (d != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
