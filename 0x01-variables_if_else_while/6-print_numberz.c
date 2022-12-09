5837cafed1e1782ce5ddba51c3f4eacf153acb4f
tarikuassefa14 <tarikuassefa14@gmail.com>
2022-12-09 08:56:37 -0800

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 48; d <= 57; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
