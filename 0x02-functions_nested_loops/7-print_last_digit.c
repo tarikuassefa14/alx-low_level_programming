8b0828582899e0a34d82dcd1982fee420cbaed6f
tarikuassefa14 <tarikuassefa14@gmail.com>
2022-12-13 13:43:24 -0800

    task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * main - This is a program that print the last digit of a number
+  *
+  * Return: Always 0 (if success)
+  */
+int print_last_digit(int k)
+{
+	int ta = k % 10;
+
+	if (k < 0)
+		ta = 0 - ta;
+	_putchar(ta + '0');
+	return (ta);
+}
