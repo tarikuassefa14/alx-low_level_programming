05fb242831412ec9f6924168095c8541aa38283d
tarikuassefa14 <tarikuassefa14@gmail.com>
2022-12-13 13:35:06 -0800

    task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * main - This is a program that computes the absolute value of an integer
+  *
+  * Return: Always 0 (if success)
+  */
+int _abs(int c)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (0 - t);
+}
