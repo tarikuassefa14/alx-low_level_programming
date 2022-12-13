9bccb1f1b49b375bec308ba7e4bb3e128b939d1f
tarikuassefa14 <tarikuassefa14@gmail.com>
2022-12-13 13:17:40 -0800

    task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * main - This is a program that print 10 times alphabet
+  *
+  * Return: Always 0 (if success)
+  */
+void print_alphabet_x10(void)
+{
+	char alp;
+	int t;
+
+	for (t = 0; t <= 9; t++)
+	{
+		for (alp = 'a'; alp <= 'z'; alp++)
+			_putchar(alp);
+		_putchar('\n');
+i	}
+}
