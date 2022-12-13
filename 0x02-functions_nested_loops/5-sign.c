6bb11b204db8cbefc7f4b686612c5b60b274964f
tarikuassefa14 <tarikuassefa14@gmail.com>
2022-12-13 13:31:23 -0800

    task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * main - This is a program that print the sign of a number
+  *
+  * Return: Always 0 (if success)
+  */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('+');
+		return (1);
+	}
+}
