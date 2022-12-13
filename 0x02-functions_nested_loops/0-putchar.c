ded78119c2864e0c81c7181809aa9bcc6c0b2c32
tarikuassefa14 <tarikuassefa14@gmail.com>
2022-12-13 12:29:44 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * main - This is a program that print putchar
+  *
+  * Return: Always 0 (if success)
+  */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
