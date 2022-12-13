a15b24b450be8915c59da569bfb3ad1e3b03bd7a
tarikuassefa14 <tarikuassefa14@gmail.com>
2022-12-13 13:26:14 -0800

    task~

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * main - This is a program that print 10 times alphabet
+  *
+  * Return: Always 0 (if success)
+  */
+int _islower(int c)
+{
+	if (c >= 'a' &&	c <= 'z')
+		return (1);
+	else
+		return (0);
+}
