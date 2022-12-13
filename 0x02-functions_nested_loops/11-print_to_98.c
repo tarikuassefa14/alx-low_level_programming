81836f849b71c1467c197b4535f5c3433c4041e2
tarikuassefa14 <tarikuassefa14@gmail.com>
2022-12-13 13:57:01 -0800

    task11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * main - This is a program that print 10 times alphabet
+  *
+  * Return: Always 0 (if success)
+  */
+void print_to_98(int la)
+{
+	if (la > 98)
+	{
+		while (la >= 98)
+		{
+			printf("%d", n);
+			if (la != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		while (la <= 98)
+		{
+			printf("%d", n);
+			if (la != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	printf("\n");
+}
