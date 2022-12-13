305659c2c367aba6dac7400f9d82bfe9ef00ef71
tarikuassefa14 <tarikuassefa14@gmail.com>
2022-12-13 13:46:37 -0800

    task8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * main - This is a program that print every minute of the day of Jack Bauer
+  *
+  * Return: Always 0 (if success)
+  */
+void jack_bauer(void)
+{
+	int q, r
+
+	for (q = 0; q < 24; q++)
+	{
+		for (r = 0; r < 60; r++)
+		{
+			_putchar(q / 10 + '0');
+			_putchar(q % 10 + '0');
+			_putchar(':');
+			_putchar(r / 10 + '0');
+			_putchar(r % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
