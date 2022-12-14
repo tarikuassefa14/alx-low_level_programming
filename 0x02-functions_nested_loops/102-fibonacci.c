1a9390a960dd78f6a8d4b3a731e080d683d8a8fc
tarikuassefa14 <tarikuassefa14@gmail.com>
2022-12-14 09:21:28 -0800

    task14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - return first 50 fabonacci 
+ *
+ * Description: print firs 50 fabonacci
+ *
+ * Return: 0 -> end of program
+ *
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long ta1 = 0, ta2 = 1, sum;
+
+	for (i = 0; i < 50; i++)
+	{
+		sum = ta1 + ta2;
+		printf("%lu", sum);
+		ta1 = ta2;
+		ta2 = sum;
+		if (i == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
