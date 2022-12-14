249e49dae8158321b7b18ec7e11f6036dc4ccdaa
tarikuassefa14 <tarikuassefa14@gmail.com>
2022-12-14 09:01:21 -0800

    task12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * print_times_table -  prints the n times table, starting with 0
- * @n: number set the limit
+ * @n: integer
  *
  * Return: void
  *
@@ -10,7 +10,7 @@
 
 void print_times_table(int n)
 {
-	int i, j, mul;
+	int i, j, tatu;
 
 	if ((n <= 15) && (n >= 0))
 	{
@@ -18,32 +18,32 @@ void print_times_table(int n)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				mul = (i * j);
+				tatu = (i * j);
 				if (j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 				}
-				if (mul < 10 && j != 0)
+				if (tatu < 10 && j != 0)
 				{
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((mul % 10) + '0');
 				}
-				else if (mul >= 10 && mul < 100)
+				else if (tatu >= 10 && tatu < 100)
 				{
 					_putchar(' ');
-					_putchar((mul / 10) + '0');
-					_putchar((mul % 10) + '0');
+					_putchar((tatu / 10) + '0');
+					_putchar((tatu % 10) + '0');
 				}
-				else if (mul >= 100 && j != 0)
+				else if (tatu >= 100 && j != 0)
 				{
-					_putchar((mul / 100) + '0');
-					_putchar((mul / 10) % 10 + '0');
-					_putchar((mul % 10) + '0');
+					_putchar((tatu / 100) + '0');
+					_putchar((tatu / 10) % 10 + '0');
+					_putchar((tatu % 10) + '0');
 				}
 				else
-					_putchar((mul % 10) + '0');
+					_putchar((tatu % 10) + '0');
 			}
 			_putchar('\n');
 		}
