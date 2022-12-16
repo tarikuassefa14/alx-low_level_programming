f0c17ea1816a711e3f7f272201cee51853d197c0
tarikuassefa14 <tarikuassefa14@gmail.com>
2022-12-16 06:13:15 -0800

    task03

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - print the number from 0 - 9.
+*
+* Return: Always 0 (if success)
+*/
+
+void print_numbers(void)
+{i
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
