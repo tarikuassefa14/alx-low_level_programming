3491eed180396bbc1dc95cc385b03a0c70a2ade6
tarikuassefa14 <tarikuassefa14@gmail.com>
2023-01-03 12:57:47 -0800

    fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *set_string - set value of pointer to a char
+ *
+ * @s: String source
+ *
+ * @to: Adress
+ *
+ * Return: Nothing
+ *
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
