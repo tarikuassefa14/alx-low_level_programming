ae3fe0c3964e8708381a16949dc63e32df71b991
tarikuassefa14 <tarikuassefa14@gmail.com>
2023-01-03 12:27:51 -0800

    string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring.
+ *
+ * @haystack: first string 
+ *
+ * @needle: second string 
+ *
+ * Return: Null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = haystack, *fneedle = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack++ != *needle++)
+			{
+				break;
+			}
+		}
+		if (!*needle)
+		{
+			return (result);
+		}
+		needle = fneedle;
+		result++;
+		haystack = result;
+	}
+	return (0);
+}
