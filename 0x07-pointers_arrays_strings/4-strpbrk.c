3e663f7b567d6dd686d57f78ee219076c5de7f1c
tarikuassefa14 <tarikuassefa14@gmail.com>
2023-01-03 12:22:34 -0800

    search

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ *
+ * @s: String
+ *
+ * @accept: Character
+ *
+ * Return: NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int x;
+
+	while (*s)
+	{
+		for (x = 0; accept[x]; x++)
+		{
+			if (*s == accept[x])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (0);
+}
