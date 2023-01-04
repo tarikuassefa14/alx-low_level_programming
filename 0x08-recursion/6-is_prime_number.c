c4745184313419b81a94715a2c580dc04f5f1851
tarikuassefa14 <tarikuassefa14@gmail.com>
2023-01-04 09:03:25 -0800

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * is_prim - function that returns the power of number
+ *
+ * @n: This is number
+ *
+ * @y: This is counter
+ *
+ * Return: Prime value
+ */
+
+int is_prime(int n, int y)
+{
+	if (n == 1)
+		return (0);
+	else if (n % y == 0 && y != n && y != 1)
+		return (0);
+	else if (n % y == 0 && y == n)
+		return (1);
+	else
+		return (is_prime(n, y + 1));
+}
+
+/**
+* is_prime_number - check the  prime number
+*
+* @n: Checked number 
+*
+* Return: Show an int 1 prime or an int 0 not prime
+*/
+
+int is_prime_number(int n)
+{
+	return (is_prime(n, 1));
+}
