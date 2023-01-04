fbeb9fddb034b536e517e5f116efd0229b792505
tarikuassefa14 <tarikuassefa14@gmail.com>
2023-01-04 09:31:56 -0800

    nothing

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings and return one if the string is idetical
+ *
+ * @s1: Pointer ot string one
+ *
+ * @s2: Pointer tp string two
+ *
+ * Return: an int 1 if True
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
+
