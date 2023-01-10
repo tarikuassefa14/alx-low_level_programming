47d20fc0045ba1c3123aff9ae706dcf8ab4030a4
tarikuassefa14 <tarikuassefa14@gmail.com>
2023-01-10 04:16:27 -0800

    concatenates

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,31 +10,39 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *p;
-	int x = 0, y = 0, z = 0;
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 != NULL)
-		for (; s1[i]; x++)
-		;
-	if (s2 != NULL)
-		for (; s2[j]; y++)
-		;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	p = malloc(sizeof(char) * (x + y + 1));
-	if (p == NULL)
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
 		return (NULL);
 
-	while (z < i)
+	i = 0;
+	j = 0;
+
+	if (s1)
 	{
-		p[z] = s1[z];
-		z++;
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
 	}
 
-	while (z < x + y)
+	if (s2)
 	{
-		p[z] = s2[z - i];
-		z++;
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
 	}
-	p[z] = '\0';
-	return (p);
+	s3[i] = '\0';
+	return (s3);
 }
