5b34251aa8fc9c6db33d6a01a1bbf6791e41f3b3
tarikuassefa14 <tarikuassefa14@gmail.com>
2023-01-10 02:12:55 -0800

    array

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - A program that prints its name, then a new line.
+ *@argc: array length.
+ *@argv: aragumen of an array.
+ *Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
