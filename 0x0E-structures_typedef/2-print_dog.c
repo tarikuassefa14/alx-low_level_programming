72ea95ebcf9d28a452f78eb5aab40b34a4ac4c2d
tarikuassefa14 <tarikuassefa14@gmail.com>
2023-01-16 05:10:39 -0800

    dog attrib

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,13 +8,13 @@
  *
  * Return: Null
  */
+
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\nAge: %f\nOwner: %s\n",
-		       d->name == NULL ? d->name : "(nil)");
-		       ((d->age) ? d->age : 0);
-		       ((d->owner) ? d->owner : "(nil)");
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
 }
