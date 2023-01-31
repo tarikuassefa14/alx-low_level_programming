45c665b9a06bceaa648505f8b9728880833139f5
tarikuassefa14 <tarikuassefa14@gmail.com>
2023-01-31 12:18:14 -0800

    fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t x = 0;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		x++;
+		i++;
 	}
-	return (x);
+	return (i);
 }
