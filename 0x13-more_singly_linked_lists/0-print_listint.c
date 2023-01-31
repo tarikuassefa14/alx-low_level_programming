c5cbab93188aece54258f1e09366c35ffb588bc1
tarikuassefa14 <tarikuassefa14@gmail.com>
2023-01-31 12:19:19 -0800

    fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * print_listint - Prints all the elements of a listint_t list
+ * print_listint - prints all the elements of a listint_t list
  *
- * @h: Linklist node head
+ * @h: head of linklist node
  *
- * Return: Nodes of the number
+ * Return: the number of nodes
  */
 
 size_t print_listint(const listint_t *h)
