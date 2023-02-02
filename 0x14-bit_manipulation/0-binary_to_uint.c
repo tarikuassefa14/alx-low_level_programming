f127ad9d2151bef996a66e71ea48afdaceaf5c45
tarikuassefa14 <tarikuassefa14@gmail.com>
2023-02-02 12:32:57 -0800

    fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,18 +10,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int decimal_v = 0;
+	unsigned int num = 0;
+	int len = 0;
 
-	if (!b)
+	if (b[len] == '\0')
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	while ((b[len] == '0') || (b[len] == '1'))
 	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		decimal_v = 2 * decimal_v + (b[i] - '0');
+		num <<= 1;
+		num += b[len] - '0';
+		len++;
 	}
 
-	return (decimal_v);
+	return (num);
 }
